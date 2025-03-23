e99fd077314414fafed9a1a831c3c77f1ca0000e
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-23 11:42:34 -0400

    Update the license

--- script/NoteRibbon.user.js
+++ script/NoteRibbon.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         NoteRibbon
 // @namespace    Unseeable871's Stuff
-// @version      v0.0.0-a1_5
+// @version      v0.0.0-a1_6
 // @description  Replaces Noteflight's palette in the score editor with a fully functional ribbon, similar to the ribbon in Windows 8, Windows 8.1, Windows 10, and Microsoft Office apps.
 // @author       Colton Stone
+// @license      GPL-3.0-or-later
 // @tag          productivity
 // @tag          utilities
 // @match        https://www.noteflight.com/scores/view/*
