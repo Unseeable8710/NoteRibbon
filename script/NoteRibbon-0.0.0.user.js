5098ac39ae0d50b180b23388f51f2a292f54e62e
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-23 11:11:36 -0400

    Update URLs

--- script/NoteRibbon-0.0.0.user.js
+++ script/NoteRibbon-0.0.0.user.js
@@ -9,7 +9,7 @@
 // @match        https://www.noteflight.com/scores/view/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=noteflight.com
 // @grant        GM_addStyle
-// @require      https://raw.githubusercontent.com/Unseeable8710/NoteRibbon/refs/heads/main/script/noteribbon.js
-// @downloadURL  https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/script/NoteRibbon-0.0.0.user.js
-// @updateURL    https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/script/NoteRibbon-0.0.0.user.js
+// @require      https://raw.githubusercontent.com/Unseeable8710/NoteRibbon/refs/heads/main/source/noteribbon.js
+// @downloadURL  https://raw.githubusercontent.com/Unseeable8710/NoteRibbon/refs/heads/main/script/NoteRibbon-0.0.0.user.js
+// @updateURL    https://raw.githubusercontent.com/Unseeable8710/NoteRibbon/refs/heads/main/script/NoteRibbon-0.0.0.user.js
 // ==/UserScript==
