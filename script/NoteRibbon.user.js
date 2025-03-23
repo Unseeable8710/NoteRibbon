58bbafb3575de31b8585917413a4140a434215e1
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-23 12:35:00 -0400

    Grant unsafeWindow

--- script/NoteRibbon.user.js
+++ script/NoteRibbon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NoteRibbon
 // @namespace    Unseeable871's Stuff
-// @version      v0.0.0-a1_13
+// @version      v0.0.0-a1_14
 // @description  Replaces Noteflight's palette in the score editor with a fully functional ribbon, inspired to the ribbon in Windows 8, Windows 8.1, Windows 10, and Microsoft Office apps.
 // @author       Colton Stone
 // @license      GNU GPL v3.0
@@ -10,10 +10,11 @@
 // @match        *://www.noteflight.com/scores/view/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=noteflight.com
 // @grant        GM_addStyle
-// @grant        GM_getValue
-// @grant        GM_setValue
 // @grant        GM_deleteValue
+// @grant        GM_getValue
 // @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        unsafeWindow
 // @require      https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/source/noteribbon.js
 // @downloadURL  https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/script/NoteRibbon.user.js
 // @updateURL    https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/script/NoteRibbon.user.js
