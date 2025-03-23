0aa1e910afcf0484435741636bdb2238c21ec878
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-23 17:15:56 -0400

    test

--- source/noteribbon.js
+++ source/noteribbon.js
@@ -7,4 +7,5 @@ GM_registerMenuCommand("Open NoteRibbon Settings", showHideSettings);
 (function() {
   'use strict';
   const scorePalette = document.getElementById("scorePalette");
+  console.log(scorePalette);
 })();
