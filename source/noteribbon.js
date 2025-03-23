cfc5cbf2be019e5f58b281b9a76ae964c9af75bf
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-23 17:23:00 -0400

    Test

--- source/noteribbon.js
+++ source/noteribbon.js
@@ -1,13 +1,33 @@
+var uwind = uwindow = unsafeWindow;
 function showHideSettings() {
   console.log("Menu command executed");
 }
-var uwind = uwindow = unsafeWindow;
+function waitForElm(selector) {
+  return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+          return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+          if (document.querySelector(selector)) {
+              observer.disconnect();
+              resolve(document.querySelector(selector));
+          }
+      });
+
+      // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+      observer.observe(document.body, {
+          childList: true,
+          subtree: true
+      });
+  });
+}
 
 GM_registerMenuCommand("Open NoteRibbon Settings", showHideSettings);
 (function() {
   'use strict';
   setTimeout(() => {
-    const scorePalette = document.getElementById("scorePalette");
+    const scorePalette = waitForElm("#scorePalette");
     console.log(scorePalette);
   }, 10000);
 })();
