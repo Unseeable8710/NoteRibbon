813b397eef11030ce9197d5511b29d0c15a22b56
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-23 17:15:43 -0400

    Test

--- source/noteribbon.js
+++ source/noteribbon.js
@@ -1,8 +1,10 @@
 function showHideSettings() {
   console.log("Menu command executed");
 }
+var uwind = uwindow = unsafeWindow;
 
 GM_registerMenuCommand("Open NoteRibbon Settings", showHideSettings);
 (function() {
   'use strict';
+  const scorePalette = document.getElementById("scorePalette");
 })();
