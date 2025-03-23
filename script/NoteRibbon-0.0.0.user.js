0c3cb04439b51899e0ef78ebe586ae0b2f5fee4d
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-23 10:16:34 -0400

    The Great File Shift

---
+++ script/NoteRibbon-0.0.0.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         NoteRibbon
+// @namespace    Unseeable871
+// @version      0.0.0-a1
+// @description  Replaces Noteflight's palette in the score editor with a fully functional ribbon, similar to the ribbon in Windows 8, Windows 8.1, Windows 10, and Microsoft Office apps.
+// @author       Colton Stone
+// @tag          productivity
+// @tag          utilities
+// @match        https://www.noteflight.com/scores/view/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=noteflight.com
+// @grant        GM_addStyle
+// @require      https://raw.githubusercontent.com/Unseeable8710/NoteRibbon/refs/heads/main/script/noteribbon.js
+// ==/UserScript==
