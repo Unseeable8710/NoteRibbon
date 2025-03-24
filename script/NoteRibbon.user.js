63540ad6e1fa27d877b835823919c870ca058430
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-23 22:00:43 -0400

    Added files for the ribbon

--- script/NoteRibbon.user.js
+++ script/NoteRibbon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NoteRibbon
 // @namespace    Unseeable871's Stuff
-// @version      v0.1.0-b1_37
+// @version      v0.1.0-b1_38
 // @description  Replaces Noteflight's palette in the score editor with a fully functional ribbon, inspired by the ribbon in Windows 8, Windows 8.1, Windows 10, and Microsoft Office apps.
 // @author       Colton Stone
 // @license      GNU GPL v3.0
@@ -16,6 +16,7 @@
 // @grant        GM_setValue
 // @grant        unsafeWindow
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @require      https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/source/ribbon.js
 // @require      https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/source/settings.js
 // @require      https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/source/noteribbon.js
 // @downloadURL  https://github.com/Unseeable8710/NoteRibbon/raw/refs/heads/main/script/NoteRibbon.user.js
---
+++ source/ribbon.js
@@ -0,0 +1,1 @@
+const settings = document.getElementById("ribbon").innerHTML;
