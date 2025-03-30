e89b0298381530a05978f38f6327f337aeeb0e34
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-30 16:44:45 -0400

    Added style-removing logic

--- source/ribbon.js
+++ source/ribbon.js
@@ -1,4 +1,6 @@
 const ribbon = document.getElementById("ribbon").innerHTML;
+var cssDoc = getStyleSheet("webapp-f5f4ac4e41f43f9d9ca445ed9591e5a8");
+var sheet = cssDoc.sheet ? cssDoc.sheet : cssDoc.styleSheet;
 
 function getStyleSheet(title) {
   for(var i = 0; i < document.styleSheets.length; i++) {
@@ -9,4 +11,22 @@ function getStyleSheet(title) {
   }
 }
 
-getStyleSheet("webapp-f5f4ac4e41f43f9d9ca445ed9591e5a8");
+function delCssRule(rule) {
+  if (sheet.cssRules) {
+    for (var i = 0; i < sheet.cssRules.length; i++) {
+      if (sheet.cssRules[i].selectorText === rule) {
+        // console.log(sheet.cssRules[i]);
+        sheet.deleteRule (i);
+      }
+    }
+  } else {
+    for (var i = 0; i < sheet.rules.length; i++) {
+      if (sheet.rules[i].selectorText === rule) {
+        // console.log(sheet.cssRules[i]);
+        sheet.removeRule (i);
+      }
+    }
+  }
+}
+
+delCssRule(".paletteElement");
