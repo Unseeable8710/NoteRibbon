8db2fc2926f85d07d95d10921370386e642687f4
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-03-31 20:43:49 -0400

    Continued work on the tabs

--- source/ribbon.js
+++ source/ribbon.js
@@ -1,6 +1,36 @@
 const ribbon = document.getElementById("ribbon").innerHTML;
+const fileTab = document.getElementById("fileTab");
+const editTab = document.getElementById("editTab");
+const durationTab = document.getElementById("durationTab");
+const rhythmTab = document.getElementById("rhythmTab");
+const pitchTab = document.getElementById("pitchTab");
+const tempoTab = document.getElementById("tempoTab");
+const textTab = document.getElementById("textTab");
+const measureTab = document.getElementById("measureTab");
+const layoutTab = document.getElementById("layoutTab");
+const bracketsTab = document.getElementById("bracketsTab");
+const repeatTab = document.getElementById("repeatTab");
+const dynamicsTab = document.getElementById("dynamicsTab");
+const articulationTab = document.getElementById("articulationTab");
+const ornamentTab = document.getElementById("ornamentTab");
+const noteTab = document.getElementById("noteTab");
+const voiceTab = document.getElementById("voiceTab");
+const tabTab = document.getElementById("tabTab");
+const linesTab = document.getElementById("linesTab");
+const colorTab = document.getElementById("colorTab");
+const soundTab = document.getElementById("soundTab");
+const viewTab = document.getElementById("viewTab");
+const tabs = [fileTab, editTab, durationTab, rhythmTab, pitchTab, tempoTab, textTab, measureTab, layoutTab, bracketsTab, repeatTab, dynamicsTab, articulationTab, ornamentTab, noteTab, voiceTab, tabTab, linesTab, colorTab, soundTab, viewTab];
+var state = false;
 var cssDoc = getStyleSheet("webapp-f5f4ac4e41f43f9d9ca445ed9591e5a8");
 var sheet = cssDoc.sheet ? cssDoc.sheet : cssDoc.styleSheet;
+
+for (var i = 0; i < tabs.length; i++) {
+  var tab = tabs[i];
+  tab.addEventListener("click", (e) => {
+    e.target.
+  });
+}
 
 function getStyleSheet(title) {
   for(var i = 0; i < document.styleSheets.length; i++) {
